acb1572a5f31dad001b4d97c75a5615046e909f0
Daniel Mendonca <danielwilliam1204@gmail.com>
2026-02-23 09:45:29 -0300

    cenario 2.1.5.py

---
+++ cenario.py
@@ -0,0 +1,2 @@
+rint("Olá, Python!") print("Danico") # erro 1 (removendo aspas) → NameError # print(Danico) # erro 2 (removendo parênteses) → SyntaxError # print "Danico" print('Teste com aspas simples') print("Olá"); print("Python") print("Olá\nPython") #5
+Open
