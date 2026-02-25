d68270e3d7661cdf70d181502ffb2ac74cb678dd
Daniel Mendonca <danielwilliam1204@gmail.com>
2026-02-25 09:47:27 -0300

    operacoes simples.py

---
+++ operacoes simples.py
@@ -0,0 +1,8 @@
+n1 = float(input("Digite um número: ")) #base do primeiro digito!!
+n2 = float(input("Digite outro número: ")) #base do segundo digito!!
+
+print("Soma:", n1 + n2)
+print("Subtração:", n1 - n2)
+print("Multiplicação:", n1 * n2)
+print("Divisão:", n1 / n2 if n2 != 0 else "definitamente, n da pra dividir por zero")
+print("Exponenciação:", n1 ** n2)
